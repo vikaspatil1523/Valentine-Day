776397643385486fb6838ed415290b4481216629
vikaspatil1523 <vikaspatil0515@gmail.com>
2025-02-13 09:52:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Sona'; // Replace with your secret code
+const secretCode = 'Aarti'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const userInput = codeInput.value.trim();
 	if (userInput === secretCode) {
-		resultDiv.innerHTML = 'Baby';
+		resultDiv.innerHTML = 'Sanuu';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
-		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai meri sona';
+		resultDiv.innerHTML = 'Nahi Tu Majhu Aaru nahi aahes majhi aaru kuth aahe';
 	}
 });
